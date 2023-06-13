6d5aed18ca64e8a62bff6cdce628fdf0d2225339
Станислав Столбенников <sstolbennikov@smart-consulting.ru>
2023-06-14 00:57:34 +0700

    Global settings

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import SseButton from "./components/sse-button/sse-button.component";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SseButton />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul>
+                    <li>My page</li>
+                    <li>Community</li>
+                    <li>Random joke</li>
+                    <li>About project</li>
+                </ul>
+            </header>
+            <main className="App-main">
+                Main
+                <SseButton/>
+            </main>
+            <footer className="App-footer">
+                Footer
+            </footer>
+        </div>
+    );
 }
 
 export default App;
